9f4fde2213c245d8570734a9d96d9045ed9ed203
Muhammad Mustafa <muhammadidev@outlook.com>
2024-02-04 20:51:29 +0200

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: path.resolve(__dirname, "./src/components"),
+      views: path.resolve(__dirname, "./src/views"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+    },
+  },
+});
